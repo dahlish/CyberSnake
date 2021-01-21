33cb62b44de4aedb27242851cfff523264d5b5b2
zqualltechnologies <75486197+zqualltechnologies@users.noreply.github.com>
2021-01-21 22:07:44 +0100

    Övningsuppgifter Del 2

---
+++ CSharp/Practice2/Practice2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice2
+{
+    public class Program
+    {
+        public static int correctAnswer = 0;
+        public static bool gameActive = false;
+        static void Main(string[] args)
+        {
+            IsPalindrome(22223);
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            string reversedWord = "";
+            
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+
+            return (word == reversedWord);
+        }
+
+        public static bool IsPalindrome(int numbers)
+        {
+            List<int> numberList = new List<int>();
+            int numberCounter = numbers;
+            int reversedInt = 0;
+            while (numberCounter > 0)
+            {
+                numberList.Add(numberCounter % 10);
+                numberCounter /= 10;
+            }
+
+            foreach (var number in numberList)
+            {
+                reversedInt = 10 * reversedInt + number;
+            }
+
+            return (numbers == reversedInt);
+            
+        }
+
+        public static string GuessingGame(int guess)
+        {
+            if (!gameActive)
+            {
+                gameActive = true;
+                Random rand = new Random();
+                correctAnswer = rand.Next(1, 100);
+            }
+
+            if (guess == correctAnswer)
+            {
+                return "Rätt!";
+            }
+            else if (guess > correctAnswer)
+            {
+                return "För högt!";
+            }
+            else
+            {
+                return "För lågt!";
+            }
+        }
+    }
+}
---
+++ CSharp/Practice2/Practice2Tests/ProgramTests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practice2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            string word = "tillit";
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest2()
+        {
+            string word = "is";
+            bool expected = false;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsPalindromeTest3()
+        {
+            string word = "ö";
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsPalindromeTest4()
+        {
+            string word = "kajak";
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest5()
+        {
+            string word = "apa";
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsPalindromeTest6()
+        {
+            string word = "monster";
+            bool expected = false;
+
+            bool actual = Program.IsPalindrome(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest7()
+        {
+            int number = 22222;
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest8()
+        {
+            int number = 22322;
+            bool expected = true;
+
+            bool actual = Program.IsPalindrome(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest9()
+        {
+            int number = 22323;
+            bool expected = false;
+
+            bool actual = Program.IsPalindrome(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest10()
+        {
+            int number = 62616367;
+            bool expected = false;
+
+            bool actual = Program.IsPalindrome(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GuessingGameTest()
+        {
+            Random rand = new Random();
+            Program.GuessingGame(0);
+            int guess = rand.Next(1, 100);
+            string expected = "Rätt!";
+            if (guess == Program.correctAnswer)
+            {
+                expected = "Rätt!";
+            }
+            else if (guess > Program.correctAnswer)
+            {
+                expected = "För högt!";
+            }
+            else
+            {
+                expected = "För lågt!";
+            }
+
+            string actual = Program.GuessingGame(guess);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
