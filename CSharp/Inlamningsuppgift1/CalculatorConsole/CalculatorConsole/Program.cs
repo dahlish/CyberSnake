a20e56bfbed8b7f9f1f5a45267ccc5751771808a
zqualltechnologies <75486197+zqualltechnologies@users.noreply.github.com>
2021-02-07 11:11:11 +0100

    Finished calculator app

--- CSharp/Inlamningsuppgift1/CalculatorConsole/CalculatorConsole/Program.cs
+++ CSharp/Inlamningsuppgift1/CalculatorConsole/CalculatorConsole/Program.cs
@@ -3,41 +3,48 @@ using System.Collections.Generic;
 
 namespace CalculatorConsole
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
+        static string currentCalculation = "";
 
+        public static void Main(string[] args)
+        {
             List<string> calculations = new List<string>();
             bool keepRunning = true;
-
             do
             {
                 Console.WriteLine("Här är dina alternativ:\nAddition: +\nSubtraktion: -\nDivision: /\nMulitplikation: *" +
-                    "\nTemperaturomvandling: konvertera\nLista alla uträkningar: lista");
+                    "\nTemperaturomvandling: konvertera\nNewtons andra lag: newton\nBMI-uträkning: bmi\nLista alla uträkningar: lista\nAvsluta: quit");
 
                 string menyVal = Console.ReadLine().ToLower();
                 switch (menyVal)
                 {
                     case "+":
-                        int addNum1 = GetNumber("Mata in det första numret: ");
-                        int addNum2 = GetNumber("Mata in det andra numret: ");
+                        float addNum1 = GetNumber("Mata in det första numret: ");
+                        currentCalculation += "+ ";
+                        float addNum2 = GetNumber("Mata in det andra numret: ");
 
                         float addResult = Compute(addNum1, addNum2, "+");
                         Console.WriteLine("Resultat: " + addResult);
-                        calculations.Add($"{addNum1} + {addNum2} => {addResult}");
+                        currentCalculation += $"=> {addResult}";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
                         break;
                     case "-":
-                        int subNum1 = GetNumber("Mata in det första numret: ");
-                        int subNum2 = GetNumber("Mata in det andra numret: ");
+                        float subNum1 = GetNumber("Mata in det första numret: ");
+                        currentCalculation += "- ";
+                        float subNum2 = GetNumber("Mata in det andra numret: ");
 
                         float subResult = Compute(subNum1, subNum2, "-");
                         Console.WriteLine("Resultat: " + subResult);
-                        calculations.Add($"{subNum1} - {subNum2} => {subResult}");
+                        currentCalculation += $"=> {subResult}";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
                         break;
                     case "/":
-                        int divNum1 = GetNumber("Mata in det första numret: ");
-                        int divNum2 = GetNumber("Mata in det andra numret: ");
+                        float divNum1 = GetNumber("Mata in det första numret: ");
+                        currentCalculation += "/ ";
+                        float divNum2 = GetNumber("Mata in det andra numret: ");
                         float divResult;
                         try
                         {
@@ -46,31 +53,37 @@ namespace CalculatorConsole
                         catch (DivideByZeroException)
                         {
                             Console.WriteLine("Du försökte dela med 0! Vill du spränga världen eller?");
+                            currentCalculation = "";
                             break;
                         }
 
                         Console.WriteLine("Resultatet är: " + divResult);
-                        calculations.Add($"{divNum1} / {divNum2} => {divResult}");
+                        currentCalculation += $"=> {divResult}";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
                         break;
                     case "*":
-                        int mulNum1 = GetNumber("Mata in det första numret: ");
-                        int mulNum2 = GetNumber("Mata in det andra numret: ");
+                        float mulNum1 = GetNumber("Mata in det första numret: ");
+                        currentCalculation += "* ";
+                        float mulNum2 = GetNumber("Mata in det andra numret: ");
 
                         float mulResult = Compute(mulNum1, mulNum2, "*");
                         Console.WriteLine("Resultat: " + mulResult);
-                        calculations.Add($"{mulNum1} * {mulNum2} => {mulResult}");
+                        currentCalculation += $"=> {mulResult}";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
                         break;
 
                     case "konvertera":
                         Console.WriteLine("Vilken enhet vill du konvertera ifrån: C/F?");
                         string tempOp = Console.ReadLine().ToUpper();
-                        float temp = GetFloat($"Ange temperatur i {tempOp}: ");
+                        float temp = GetNumber($"Ange temperatur i {tempOp}: ");
                         float convertResult = default;
                         string newType;
+
                         //Detta hade jag velat ha gjort redan i konverteringsmetoden, funderade på att använda en class Temperature
                         //istället men kände att det hade frångått för mycket från övriga lösningar vilket kändes opassande och ej enhetligt.
-                        //Ett annat alternativ hade varit kanske att lagra den nya enheten i en variabel i klass-scopet men det känns
-                        //som bad practice i och med att jag kanske skulle vilja använda denna metod i ett annat program någon gång.
+                        //Ett annat alternativ hade varit kanske att lagra den nya enheten i en variabel i klass-scopet som jag gjort med currentCalculation, men jag lät detta vara såhär nu.
                         if (tempOp == "C")
                         {
                             newType = "F";
@@ -86,74 +99,86 @@ namespace CalculatorConsole
                         catch (FormatException)
                         {
                             Console.WriteLine("Du kan inte konvertera från detta.");
+                            currentCalculation = "";
+                            break;
                         }
 
                         Console.WriteLine($"Resultat: {convertResult} {newType}");
-                        calculations.Add($"{temp} {tempOp} => {convertResult} {newType}");
+                        currentCalculation += $"{tempOp} => {convertResult} {newType}";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
                         break;
+                    case "newton":
+                        float mass = GetNumber("Ange massa (kg): ");
+                        currentCalculation += "* ";
+                        float acceleration = GetNumber("Ange acceleration (m/s): ");
+
+                        float newtonResult = Compute(mass, acceleration, "*");
+                        Console.WriteLine($"Resultat: {newtonResult} N");
+                        currentCalculation += $"=> {newtonResult} N";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
+                        break;
+                    case "bmi":
+                        float weight = GetNumber("Ange vikt (kg): ");
+                        currentCalculation += "kg & ";
+                        float height = GetNumber("Ange längd (cm): ");
+                        currentCalculation += "cm => ";
+                        float bmiResult = default;
+                        try
+                        {
+                            bmiResult = CalculateBmi(weight, height);
+                        }
+                        catch(DivideByZeroException)
+                        {
+                            Console.WriteLine("Din längd får inte vara 0.");
+                            currentCalculation = "";
+                            break;
+                        }
+                        catch (ArgumentOutOfRangeException)
+                        {
+                            Console.WriteLine("Din längd kan inte vara negativ.");
+                            currentCalculation = "";
+                            break;
+                        }
+                        Console.WriteLine($"Resultat: {bmiResult} BMI");
+                        currentCalculation += $"{bmiResult} BMI";
+                        calculations.Add(currentCalculation);
+                        currentCalculation = "";
+                        break;
+
                     case "lista":
                         ListAllCalculations(calculations);
+                        break;
+                    case "quit":
+                        keepRunning = false;
                         break;
                     default:
                         Console.WriteLine("Okänt menyval. Försök igen.");
                         break;
                 }
 
-                Console.WriteLine("\n");
+                Console.WriteLine();
             } while (keepRunning);
         }
 
         /// <summary>
-        /// Asks the user to enter a number value and then returns it as an int. If the user enters a different
-        /// value than a number, the method will be executed again and a message will be shown in the console.
-        /// The message parameter is optional and will be output to the console should it else than null or empty.
-        /// </summary>
-        /// <param name="message">Add an optional message to be shown to the console before the input is asked for.</param>
-        /// <returns>An int with the input value.</returns>
-        public static int GetNumber(string message = "")
-        {
-            int result = default;
-            if (!string.IsNullOrEmpty(message))
-            {
-                Console.WriteLine(message);
-            }
-            string inputString = Console.ReadLine();
-
-            if (inputString.ToLower() == "marcus")
-            {
-                inputString = "42";
-            }
-
-            try
-            {
-                result = int.Parse(inputString);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Du har inte matat in ett nummer. Vänligen försök igen.");
-                GetNumber();
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Asks the user to enter a floating point value and then returns it as a float. If the user enters a different
-        /// value than a number, the method will be executed again and a message will be shown in the console.
+        /// Asks the user to enter a numeric value and then returns it as a float. If the user enters a different
+        /// value than a numeric value, the method will be executed again and a message will be shown in the console.
         /// The message parameter is optional and will be output to the console should it else than null or empty.
         /// </summary>
         /// <param name="message">Add an optional message to be shown to the console before the input is asked for.</param>
         /// <returns>A float with the input value.</returns>
-        public static float GetFloat(string message = "")
+        public static float GetNumber(string message = "")
         {
             float result = default;
-
             if (!string.IsNullOrEmpty(message))
             {
                 Console.WriteLine(message);
             }
-
             string inputString = Console.ReadLine();
+            inputString = inputString.Replace('.', ',');
+            currentCalculation += $"{inputString} ";
 
             if (inputString.ToLower() == "marcus")
             {
@@ -162,13 +187,12 @@ namespace CalculatorConsole
 
             try
             {
-                inputString = inputString.Replace('.', ',');
                 result = float.Parse(inputString);
             }
             catch (FormatException)
             {
-                Console.WriteLine("Du har inte matat in ett nummer. Vänligen försök igen.");
-                GetFloat();
+                currentCalculation = currentCalculation.Replace($"{inputString} ", "");
+                result = GetNumber("Du har inte matat in ett nummer. Vänligen försök igen.");
             }
 
             return result;
@@ -195,7 +219,7 @@ namespace CalculatorConsole
         /// <exception cref="DivideByZeroException">If an attempt to divide by zero is made, this exception will be thrown.</exception>
         /// <exception cref="FormatException">If an unknown operator is used, this exception is thrown.</exception>
         /// <returns></returns>
-        public static float Compute(int num1, int num2, string op)
+        public static float Compute(float num1, float num2, string op)
         {
             switch (op)
             {
@@ -210,18 +234,17 @@ namespace CalculatorConsole
                     }
                     else
                     {
-                        return (float)num1 / (float)num2;
+                        return num1 / num2;
                     }
                 case "*":
                     return num1 * num2;
-
                 default:
                     throw new FormatException("An invalid operator was used. Please use +, -, * or /");
             }
         }
 
         /// <summary>
-        /// Converts temperature from celsius to fahrenheit and vice versa.
+        /// Converts temperature from Celsius to Fahrenheit and vice versa.
         /// </summary>
         /// <param name="temperature">The temperature in the old unit type.</param>
         /// <param name="convertFrom">The unit type you are converting from.</param>
@@ -238,6 +261,34 @@ namespace CalculatorConsole
                 default:
                     throw new FormatException("You have to convert from either C or F.");
             }
+        }
+
+        /// <summary>
+        /// Calculates the body mass index based on the input weight and length. Both parameters must be positive.
+        /// </summary>
+        /// <param name="weight">The weight in kilograms. Must be positive.</param>
+        /// <param name="height">The length in cm. Must be positive.</param>
+        /// <returns>The calculated BMI as a float.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If any of the parameter values are negative, this exception is thrown.</exception>
+        /// <exception cref="DivideByZeroException">If the height is 0, this exception is thrown.</exception>
+        public static float CalculateBmi(float weight, float height)
+        {
+            float bmi = 0;
+
+            if (height < 0 || weight < 0)
+            {
+                throw new ArgumentOutOfRangeException("The parameter values must be a positive number.");
+            }
+
+            height = height / 100;
+            bmi = weight / MathF.Pow(height, 2);
+
+            if (float.IsInfinity(bmi)) //Här upptäckte jag att dela en float på 0 resulterar i infinity istället för DivideByZeroException. Finns det ett bättre sätt att jobba runt detta eller är detta "rätt" sätt? :)
+            {
+                throw new DivideByZeroException("Can't divide by zero.");
+            }
+            
+            return MathF.Round(bmi, 1);
         }
     }
 }
---
+++ CSharp/Inlamningsuppgift1/CalculatorConsole/CalculatorConsoleTests/ProgramTests.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorConsole;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CalculatorConsole.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetNumberTest()
+        {
+            string test1Num1 = "30";
+            float test1Expected = 30;
+            StringReader input = new StringReader(test1Num1);
+            Console.SetIn(input);
+            float test1Actual = Program.GetNumber();
+            Assert.AreEqual(test1Expected, test1Actual);
+
+            string test2Num1 = "-20";
+            float test2Expected = -20;
+            input = new StringReader(test2Num1);
+            Console.SetIn(input);
+            float test2Actual = Program.GetNumber();
+            Assert.AreEqual(test2Expected, test2Actual);
+
+
+            string test3Num1 = "MarCus";
+            float test3Expected = 42;
+            input = new StringReader(test3Num1);
+            Console.SetIn(input);
+            float test3Actual = Program.GetNumber();
+            Assert.AreEqual(test3Expected, test3Actual);
+
+
+            //Eftersom GetNumber() är recursive vid fel input så fastnade jag på hur jag unit testar något som är recursive. Tar jättegärna en förklaring i detta så jag vet
+            //hur jag testar något som är recursive och samtidigt kräver input :)
+
+            //string test4Num1 = "Arthas";
+            //input = new StringReader(test4Num1);
+            //Console.SetIn(input);
+            //Assert.ThrowsException<FormatException>(() => Program.GetNumber());
+
+            string test4Num1 = "42.1";
+            float test4Expected = 42.1f;
+            input = new StringReader(test4Num1);
+            Console.SetIn(input);
+            float test4Actual = Program.GetNumber();
+            Assert.AreEqual(test4Expected, test4Actual);
+
+            string test5Num1 = "42,1";
+            float test5Expected = 42.1f;
+            input = new StringReader(test5Num1);
+            Console.SetIn(input);
+            float test5Actual = Program.GetNumber();
+            Assert.AreEqual(test5Expected, test5Actual);
+        }
+
+        [TestMethod()]
+        public void ComputeTest()
+        {
+            float test1Num1 = 10.2f;
+            float test1Num2 = 0.8f;
+            string test1Op = "+";
+            float test1Expected = 11f;
+            float test1Actual = Program.Compute(test1Num1, test1Num2, test1Op);
+            Assert.AreEqual(test1Expected, test1Actual);
+
+            float test2Num1 = 91f;
+            float test2Num2 = 100f;
+            string test2Op = "%";
+            Assert.ThrowsException<FormatException>(() => Program.Compute(test2Num1, test2Num2, test2Op));
+
+            float test3Num1 = -12.3f;
+            float test3Num2 = 0;
+            string test3Op = "/";
+            Assert.ThrowsException<DivideByZeroException>(() => Program.Compute(test3Num1, test3Num2, test3Op));
+
+            float test4Num1 = -5f;
+            float test4Num2 = -5f;
+            string test4Op = "-";
+            float test4Expected = 0f;
+            float test4Actual = Program.Compute(test4Num1, test4Num2, test4Op);
+            Assert.AreEqual(test4Expected, test4Actual);
+        }
+
+        [TestMethod()]
+        public void ConvertTemperatureTest()
+        {
+            float test1Num1 = 32f;
+            string test1Op1 = "F";
+            float test1Expected = 0f;
+            float test1Actual = Program.ConvertTemperature(test1Num1, test1Op1);
+            Assert.AreEqual(test1Expected, test1Actual);
+
+            float test2Num1 = 100f;
+            string test2Op = "C";
+            float test2Expected = 212f;
+            float test2Actual = Program.ConvertTemperature(test2Num1, test2Op);
+            Assert.AreEqual(test2Expected, test2Actual);
+
+            float test3Num1 = 32.5f;
+            string test3Op = "C";
+            float test3Expected = 90.5f;
+            float test3Actual = Program.ConvertTemperature(test3Num1, test3Op);
+            Assert.AreEqual(test3Expected, test3Actual);
+
+            float test4Num1 = 0f;
+            string test4Op = "K";
+            Assert.ThrowsException<FormatException>(() => Program.ConvertTemperature(test4Num1, test4Op));
+        }
+
+        [TestMethod()]
+        public void CalculateBmiTest()
+        {
+            float test1Num1 = 100f;
+            float test1Num2 = 180f;
+            float test1Expected = 30.9f;
+            float test1Actual = Program.CalculateBmi(test1Num1, test1Num2);
+            Assert.AreEqual(test1Expected, test1Actual);
+
+            float test2Num1 = 71.34f;
+            float test2Num2 = 172.2f;
+            float test2Expected = 24.1f;
+            float test2Actual = Program.CalculateBmi(test2Num1, test2Num2);
+            Assert.AreEqual(test2Expected, test2Actual);
+
+            float test3Num1 = -0.02f;
+            float test3Num2 = 30f;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.CalculateBmi(test3Num1, test3Num2));
+
+            float test4Num1 = 67.3f;
+            float test4Num2 = -0.42f;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.CalculateBmi(test4Num1, test4Num2));
+
+            float test5Num1 = -32f;
+            float test5Num2 = -132f;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Program.CalculateBmi(test5Num1, test5Num2));
+
+            float test6Num1 = 95f;
+            float test6Num2 = 0f;
+            Assert.ThrowsException<DivideByZeroException>(() => Program.CalculateBmi(test6Num1, test6Num2));
+        }
+    }
+}
